0be34e13fa65275b50791c99572ad3cad358e061
Viktoria Bozhko <viktoria.bozhko@jetbrains.com>
2020-04-15 13:17:40 +0300

    Initial commit

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,6 +1,11 @@
 public class A {
 
+    private static final String a = "Ololo";
+
     public static void main(String[] args) {
         System.out.println("pewpew");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a);
+        }
     }
 }
